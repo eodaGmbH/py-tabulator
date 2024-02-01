feaa3bf5e330dec90e37872fc0ba3a6152983c51
Stefan Kuethe <stefan.kuethe@eoda.de>
2024-02-01 19:05:06 +0100

    Refactor

--- pytabulator/__init__.py
+++ pytabulator/__init__.py
@@ -1,8 +1,3 @@
-from .shiny_bindings import (
-    output_tabulator,
-    render_data_frame,
-    render_data_frame_,
-    render_tabulator,
-)
+from .shiny_bindings import output_tabulator, render_data_frame, render_tabulator
 from .tabulator import TableOptions, Tabulator, TabulatorOptions
 from .tabulator_context import TabulatorContext
