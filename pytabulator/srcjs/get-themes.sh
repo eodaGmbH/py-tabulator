53d712b4d90b729591c0b5ca76671a267a5b07b2
Stefan Kuethe <stefan.kuethe@eoda.de>
2024-02-04 12:44:41 +0100

    Update script

--- pytabulator/srcjs/get-themes.sh
+++ pytabulator/srcjs/get-themes.sh
@@ -1,2 +1,10 @@
-wget https://unpkg.com/tabulator-tables@5.5.4/dist/css/tabulator_midnight.min.css
-wget https://unpkg.com/tabulator-tables@5.5.4/dist/css/tabulator_simple.min.css
+for base_theme in midnight modern simple site; do
+  echo $base_theme
+done
+
+for framework_theme in bootstrap3 bootstrap4 bootstrap5 bulma materialize semanticui; do
+  echo $framework_theme
+done
+
+# wget https://unpkg.com/tabulator-tables@5.5.4/dist/css/tabulator_midnight.min.css
+# wget https://unpkg.com/tabulator-tables@5.5.4/dist/css/tabulator_simple.min.css
