5c05a8e8e897f8d75905333d04412471585939d6
Stefan Kuethe <stefan.kuethe@eoda.de>
2024-02-06 09:44:25 +0100

    Fix example

--- docs/examples/getting_started/app.py
+++ docs/examples/getting_started/app.py
@@ -5,22 +5,22 @@ from shiny import App, render, ui
 
 app_ui = ui.page_fluid(
     ui.output_text_verbatim("txt", placeholder=True),
-    output_tabulator("tabylator", height=600),
+    output_tabulator("tabulator"),
 )
 
 
 def server(input, output, session):
     @render_tabulator
-    def tabylator():
+    def tabulator():
         df = pd.read_csv(
             "https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv"
         )
-        return Tabulator(df=df)
+        return Tabulator(df, table_options={"height": 311})
 
     @render.code
     async def txt():
-        print(input.tabylator_row())
-        return str(input.tabylator_row())
+        print(input.tabulator_row_clicked())
+        return str(input.tabulator_row_clicked())
 
 
 app = App(app_ui, server)
