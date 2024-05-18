57fb0395dc9eaf7186e55b8bd1956c2083f13b48
Stefan Kuethe <stefan.kuethe@eoda.de>
2024-05-19 00:59:40 +0200

    Remove dead code

--- srcjs/index.js
+++ srcjs/index.js
@@ -10,33 +10,6 @@ class TabulatorOutputBinding extends Shiny.OutputBinding {
     const widget = new TabulatorWidget(el, payload.data, payload.options);
     const table = widget.getTable();
 
-    /*
-    table.on("rowClick", function (e, row) {
-      const inputName = `${el.id}_row_clicked`;
-      console.log(inputName, row.getData());
-      Shiny.onInputChange(inputName, row.getData());
-    });
-
-    table.on("rowClick", (e, row) => {
-      const inputName = `${el.id}_rows_selected`;
-      const data = table.getSelectedRows().map((row) => row.getData());
-      console.log(inputName, data);
-      Shiny.onInputChange(inputName, data);
-    });
-
-    table.on("cellEdited", function (cell) {
-      const inputName = `${el.id}_row_edited`;
-      console.log(inputName, cell.getData());
-      Shiny.onInputChange(inputName, cell.getData());
-    });
-
-    table.on("dataFiltered", function (filters, rows) {
-      const inputName = `${el.id}_data_filtered`;
-      const data = rows.map((row) => row.getData());
-      console.log(inputName, data);
-      Shiny.onInputChange(inputName, data);
-    });
-    */
     table.on("tableBuilt", function () {
       if (payload.options.columnUpdates != null) {
         console.log("column updates", payload.options.columnUpdates);
